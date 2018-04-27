9ae3a80f2d53f094305e63ce53e25531b0c4bf79
k.kondo <k.kondo@kondo-no-MacBook-Pro.local>
2018-04-27 15:55:25 +0900

    Deckにトランプ全て定義

--- src/main/kotlin/model/Deck.kt
+++ src/main/kotlin/model/Deck.kt
@@ -1,8 +1,21 @@
 package model
 
-class Deck{
+class Deck {
 
-   public val cards = arrayOf<Card>(
+    public val cards = arrayOf<Card>(
+            Card(Suit.SPADE, 1),
+            Card(Suit.SPADE, 2),
+            Card(Suit.SPADE, 3),
+            Card(Suit.SPADE, 4),
+            Card(Suit.SPADE, 5),
+            Card(Suit.SPADE, 6),
+            Card(Suit.SPADE, 7),
+            Card(Suit.SPADE, 8),
+            Card(Suit.SPADE, 9),
+            Card(Suit.SPADE, 10),
+            Card(Suit.SPADE, 11),
+            Card(Suit.SPADE, 12),
+            Card(Suit.SPADE, 13),
             Card(Suit.HART, 1),
             Card(Suit.HART, 2),
             Card(Suit.HART, 3),
@@ -15,6 +28,32 @@ class Deck{
             Card(Suit.HART, 10),
             Card(Suit.HART, 11),
             Card(Suit.HART, 12),
-            Card(Suit.HART, 13)
-    )
+            Card(Suit.HART, 13),
+            Card(Suit.DIAMOND, 1),
+            Card(Suit.DIAMOND, 2),
+            Card(Suit.DIAMOND, 3),
+            Card(Suit.DIAMOND, 4),
+            Card(Suit.DIAMOND, 5),
+            Card(Suit.DIAMOND, 6),
+            Card(Suit.DIAMOND, 7),
+            Card(Suit.DIAMOND, 8),
+            Card(Suit.DIAMOND, 9),
+            Card(Suit.DIAMOND, 10),
+            Card(Suit.DIAMOND, 11),
+            Card(Suit.DIAMOND, 12),
+            Card(Suit.DIAMOND, 13),
+            Card(Suit.CLUB, 1),
+            Card(Suit.CLUB, 2),
+            Card(Suit.CLUB, 3),
+            Card(Suit.CLUB, 4),
+            Card(Suit.CLUB, 5),
+            Card(Suit.CLUB, 6),
+            Card(Suit.CLUB, 7),
+            Card(Suit.CLUB, 8),
+            Card(Suit.CLUB, 9),
+            Card(Suit.CLUB, 10),
+            Card(Suit.CLUB, 11),
+            Card(Suit.CLUB, 12),
+            Card(Suit.CLUB, 13)
+            )
 }
